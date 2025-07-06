961e5130a434a7be2ddb4f5e56ad41c4c957c807
Sriram-Kommmana <kommanamarirs@gmail.com>
2025-07-06 17:18:46 +0000

    Implement OTP sending functionality and add ApiError utility class

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,1 +1,16 @@
- 
+import {User} from '../models/User.js';
+import {Otp} from '../models/Otp.js'
+import {asynsHandler} from '../utils/asyncHandler.js';
+import {ApiResponse} from '../utils/apiResponse.js';
+import {ApiError} from '../utils/ApiError.js'
+
+const sendOTP = asynsHandler(async(req, res) => {
+    const {email} = req.body;
+
+    const checkUserPresent = await User.findOne({email});
+
+})
+
+export {
+    sendOTP
+}
